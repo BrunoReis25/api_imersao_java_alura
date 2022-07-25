027982382ae8d76201e68978067eeee8d5126542
BrunoReis25 <pessoadosreisbruno@gmail.com>
2022-07-25 11:10:24 -0300

    commit inicial

---
+++ src/test/java/br/com/projeto/linguagens/api/LinguagensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.linguagens.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
